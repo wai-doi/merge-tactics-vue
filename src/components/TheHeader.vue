0fb1ecb7a7a8e0c5e5604fd5ec3852be1a946f3f
Yusuke Doi <yuusukedoi23@gmail.com>
2025-07-12 21:50:34 +0900

    feat: add github link to header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,13 @@
+<script setup>
+import { Icon } from '@iconify/vue';
+</script>
+
 <template>
   <header class="header">
     <h1>クラロワ マージタクティクス チームビルダー</h1>
+    <a href="https://github.com/wai-doi/merge-tactics-vue" target="_blank" rel="noopener noreferrer" class="github-link">
+      <Icon icon="fa:github" />
+    </a>
   </header>
 </template>
 
@@ -11,9 +18,19 @@
   padding: 10px 20px;
   text-align: center;
   border-bottom: 1px solid #eee;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
 }
 h1 {
   font-size: 1.8em;
   margin: 0;
+}
+.github-link {
+  position: absolute;
+  right: 20px;
+  font-size: 2em;
+  color: #333;
 }
 </style>
